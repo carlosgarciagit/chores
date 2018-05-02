eb8444b9b680fcc1dc893994381adc9e5dc1267c
Christine Vonder Haar <vondy@mit.edu>
2018-05-01 20:57:55 -0400

    small changes to sign in
    also changed color scheme i hate orange

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,13 +1,21 @@
 Util.events(document, {
+	"DOMContentLoaded": function() {
+		Util.one("#name").focus();
+	},
+
 	"keyup": function(evt) {
+		Util.one("#error").style.display = "none"
 		if (evt.key == "Enter") {
 			//child
-			if (Util.one("#name").value == "Allie") {
+			if (Util.one("#name").value.toLowerCase() == "allie") {
 				window.open("child.html", name="_self")
 			}
 			//parent
-			if (Util.one("#name").value == "Andrew") {
+			else if (Util.one("#name").value.toLowerCase() == "andrew") {
 				window.open("parent.html", name="_self")
+			}
+			else {
+				Util.one("#error").style.display = "flex"
 			}
 		}
 	},
